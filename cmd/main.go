53e8fa5d8123de85ef3b2377395c3b9bbcfe06b0
Michael Asper <asper@utexas.edu>
2018-11-27 13:05:49 -0600

    Added Webserver functionality

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
 
 	"../pkg/parser"
 	"../pkg/rop"
@@ -12,21 +13,40 @@ var in = flag.String("input", "./test/stack0.dms", "ARM32 Binary file input")
 var out = flag.String("output", "", "Gadget output file")
 
 func main() {
-	// Parse command-line flags
-	flag.Parse()
+	fs := http.FileServer(http.Dir("static/"))
 
-	// Parse executable
+	http.Handle("/", http.StripPrefix("/", fs))
+	http.HandleFunc("/analyze", analyze)
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":80", nil)
+}
+
+func analyze(writer http.ResponseWriter, request *http.Request) {
+	flag.Parse()
 	blocks, err := parser.Parse(*in)
 	if err != nil {
 		panic(err)
 	}
-	// fmt.Printf("Trophy found %d blocks\n", len(blocks))
+	fmt.Printf("Trophy found %d blocks\n", len(blocks))
 
 	gadgets, err := rop.FindGadgets(blocks, rop.ARM)
 	if err != nil {
 		panic(err)
 	}
-	//
-	// Allow further refinement of search, generate automatic chain, or output all rop
+
 	fmt.Println(len(gadgets))
 }
+
+// func main() {
+// 	http.HandleFunc("/", handler)
+// 	http.ListenAndServe(":8080", nil)
+// Parse command-line flags
+// flag.Parse()
+
+// Parse executable
+
+//
+// Allow further refinement of search, generate automatic chain, or output all rop
+// fmt.Println(len(gadgets))
+// }
